41bf1aaae96ed93c7ad9c21492f9a3b3aba2ded6
ibrahimohid <ibrahimohid77@gmail.com>
2025-11-12 23:21:13 +0600

    router dom practice

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Products from "./pages/Products.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
+import Navbar from "./layouts/Navbar.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: '*',
+        element: <ErrorPage/>
+      }
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
